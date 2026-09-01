09fd685cb27aa40bdb3fc4016a77395ef2d19a6c
shanteshbhakare0-ui <shanteshbhakare0@gmail.com>
2026-09-01 17:38:55 +0000

    add variable declarations and console output for account details

---
+++ 01_basics/02_var.js
@@ -0,0 +1,14 @@
+const account_Id=133419
+let accountEmail="santanu13@gmail.com"
+let accountName="Santanu"
+let accountBalance=50000
+var accountpassword="Santanu@123"
+accountcity="Kolkata"
+let accountState;
+
+
+accountEmail="santanu13@outlook.com"
+accountpassword="Santanu@456"
+accouncity="Bangalore"
+
+console.table([account_Id,accountEmail,accountName,accountBalance,accountpassword,accountcity,accountState])
